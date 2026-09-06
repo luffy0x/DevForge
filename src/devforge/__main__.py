8b21f8bc83b15e1f90ab8defd0e7116a182d6c73
Luffy <wangzhi_hao@163.com>
2026-09-07 00:50:12 +0800

    feat: route claim subcommand

--- src/devforge/__main__.py
+++ src/devforge/__main__.py
@@ -1,5 +1,11 @@
-from .cli import main
+import sys
+
+from .cli import main as scan_main
+from .claim_cli import main as claim_main
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    if len(sys.argv) > 1 and sys.argv[1] == "claim":
+        sys.argv = [sys.argv[0], *sys.argv[2:]]
+        raise SystemExit(claim_main())
+    raise SystemExit(scan_main())
