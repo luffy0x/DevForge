27adedf0609e57a77f7bb7acac898e3beddcba6c
Luffy <wangzhi_hao@163.com>
2026-09-07 16:34:38 +0800

    feat: expose retry command

--- src/devforge/__main__.py
+++ src/devforge/__main__.py
@@ -3,6 +3,7 @@ import sys
 from .cli import main as scan_main
 from .claim_cli import main as claim_main
 from .contribute_cli import main as contribute_main
+from .retry_cli import main as retry_main
 
 
 if __name__ == "__main__":
@@ -12,4 +13,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "contribute":
         sys.argv = [sys.argv[0], *sys.argv[2:]]
         raise SystemExit(contribute_main())
+    if len(sys.argv) > 1 and sys.argv[1] == "retry":
+        sys.argv = [sys.argv[0], *sys.argv[2:]]
+        raise SystemExit(retry_main())
     raise SystemExit(scan_main())
